07779561496e9d23608797968f580f5995e87c7f
Lulzasaur <frost9192@gmail.com>
2018-10-19 16:00:48 -0700

    finished project

---
+++ models.js
@@ -0,0 +1,248 @@
+/** Models for Lunchly */
+
+const pg = require("pg");
+const moment = require("moment");
+
+const db = new pg.Client("postgresql://localhost/lunchly");
+db.connect();
+
+
+/** A reservation for a party */
+
+class Reservation {
+  constructor({id, customerId, numGuests, startAt, notes}) {
+    this.id = id;
+    this.customerId = customerId;
+    this.numGuests = numGuests;
+    this.startAt = startAt;
+    this.notes = notes;
+  }
+
+  /** methods for setting/getting numGuests */
+
+  set numGuests(val) {
+    if (val >= 1) this._numGuests = val;
+    else throw new Error("YOU NEED MORE THAN ONE PERSON TO MAKE A RESERVATION!!!!");
+  }
+
+  get numGuests() {
+    return this._numGuests;
+  }
+
+  /** methods for setting/getting startAt time */
+
+  set startAt(val) {
+    if (val instanceof Date && !isNaN(val)) this._startAt = val;
+    else throw new Error("Not a valid startAt.");
+  }
+
+  get startAt() {
+    return this._startAt;
+  }
+
+  get formattedStartAt() {
+    return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
+  }
+
+  /** methods for setting/getting notes (keep as a blank string, not NULL) */
+
+  set notes(val) {
+    this._notes = val || '';
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+  /** methods for setting/getting customer ID: can only set once. */
+
+  set customerId(val) {
+    if (this._customerId && this._customerId !== val)
+      throw new Error('Cannot change customer ID');
+    this._customerId = val;
+  }
+
+  get customerId() {
+    return this._customerId;
+  }
+
+  /** given a customer id, find their reservations. */
+
+  static async getReservationsForCustomer(customerId) {
+    const results = await db.query(
+          `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt", 
+           notes AS "notes"
+         FROM reservations 
+         WHERE customer_id = $1`,
+        [customerId]
+    );
+
+    return results.rows.map(row => new Reservation(row));
+  }
+
+  /** find a reservation by id. */
+
+  static async get(id) {
+    const result = await db.query(
+          `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt",
+           notes
+         FROM reservations 
+         WHERE id = $1`,
+        [id]
+    );
+
+    return new Reservation(results.row[0]);
+  }
+
+   /** save this reservation. */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+            `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+          [this.customerId, this.startAt, this.numGuests, this.notes]);
+      console.log("result", result.rows[0].id);
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+            `UPDATE reservations SET customer_id=$1, start_at=$2, num_guests=$3, notes=$4)
+             WHERE id=$5`,
+          [this.customerId, this.startAt, this.numGuests, this.notes, this.id]);
+    }
+  }
+}
+
+
+/** Customer of the restaurant. */
+
+class Customer {
+  constructor({id, firstName, lastName, phone, notes}) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.notes = notes;
+  }
+
+  /** methods for getting full names */
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  /** methods for getting/setting notes (keep as empty string, not NULL) */
+
+  set notes(val) {
+    this._notes = val || '';
+  }
+
+  get notes() {
+    return this._notes;
+  }
+
+  /** methods for getting/setting phone #. */
+
+  set phone(val) {
+    this._phone = val || null;
+  }
+
+  get phone() {
+    return this._phone;
+  }
+
+  /** find all customers. */
+
+  static async all() {
+    const results = await db.query(
+          `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes
+       FROM customers
+       ORDER BY last_name, first_name`
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+  //how do we fix below?
+  static async search(searchTerm) {
+    const results = await db.query(
+      `SELECT id, 
+        first_name AS "firstName",  
+        last_name AS "lastName", 
+        phone, 
+        notes
+       FROM customers
+       WHERE first_name LIKE '%${searchTerm}%' 
+       OR last_name LIKE '%${searchTerm}%'
+       ORDER BY last_name, first_name`
+    );
+    return results.rows.map(c => new Customer(c));
+  }
+
+  /** get a customer by ID. */
+
+  static async get(id) {
+    const results = await db.query(
+          `SELECT id, 
+         first_name AS "firstName",  
+         last_name AS "lastName", 
+         phone, 
+         notes 
+        FROM customers WHERE id = $1`,
+        [id]
+    );
+    return new Customer(results.rows[0]);
+  }
+
+  /** get all reservations for this customer. */
+
+  async getReservations() {
+    return await Reservation.getReservationsForCustomer(this.id);
+  }
+
+  /** save this customer. */
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+            `INSERT INTO customers (first_name, last_name, phone, notes)
+             VALUES ($1, $2, $3, $4)
+             RETURNING id`,
+          [this.firstName, this.lastName, this.phone, this.notes]);
+      console.log("result", result.rows[0].id);
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+            `UPDATE customers SET first_name=$1, last_name=$2, phone=$3, notes=$4
+             WHERE id=$5`,
+          [this.firstName, this.lastName, this.phone, this.notes, this.id]);
+    }
+  }
+
+  static async bestCustomers(){
+    const results = await db.query(
+      `SELECT customers.id, customers.first_name, customers.last_name, 
+      customers.phone, customers.notes, COUNT(*) 
+      FROM reservations JOIN customers 
+      ON reservations.customer_id = customers.id 
+      GROUP BY customers.id, customers.first_name, customers.last_name, 
+      customers.phone, customers.notes
+      ORDER BY COUNT DESC 
+      LIMIT 10;`);
+
+    return results.rows.map(c => c);
+  }
+}
+
+
+module.exports = {Customer, Reservation};
